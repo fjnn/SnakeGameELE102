8aecccfdea7db2f698bde336534796f2534045f4
Gizem Ates <gizemates@iyte.edu.tr>
2020-04-01 13:35:34 +0200

    Arduino code updated with Serial.println()

--- OlderProjects/VS_Serial_Advanced/VS_Serial_Advanced.ino
+++ OlderProjects/VS_Serial_Advanced/VS_Serial_Advanced.ino
@@ -1,58 +1,64 @@
-const int ledPins[] = {9,10,11};
+/*
+ * 1 potentiometer connected to A0
+ * 
+ * 3 LEDs connected to pin 9,10,11
+ * 
+ */
+
+const int ledPins[] = {9, 10, 11};
 #define POTPIN A0
 
-#define MAX_MILLIS_TO_WAIT 1000
+uint8_t receivedData[] = {0,0,0,0,0,0};  // --> uint8_t receivedData[] = {0,0,0,0,0}
 int dataSize = 6;
-
-uint8_t receivedData[] = {0,0,0,0,0,0};
-
-unsigned long starttime;
 
 void setup() {
   Serial.begin(9600);
-  for(int i=0; i<3; i++)
+  for(int i = 0; i < 3; i++)
     pinMode(ledPins[i], OUTPUT);
 }
 
 void loop() {
+
   while(Serial.available())
   {
     uint8_t headerByte = Serial.read();
+
     if(headerByte == 5)
     {
-      // we have found the starting byte
-      // check for the incoming data is valid
-      Serial.readBytes(receivedData, dataSize-1);
+      // this is my header, Found it, YAY.
+      Serial.readBytes(receivedData, dataSize);
     }
 
-    if(receivedData[0] == 1) // LED manipulation
+    if(receivedData[0] == 1) // LED Manipulation -- the first element of my sub array = 1
     {
-      if(receivedData[1] == 0) // OFF Button pressed
+      if(receivedData[1] == 0) // Button Related -- OFF Button is pressed
       {
-        for(int i=0; i<3; i++)
-          if(receivedData[i+2] == 1) 
+        for(int i = 0; i < 3; i++)
+          if(receivedData[i+2] == 1)  // Example array: {header,1,0, L1, L2, L3}
             digitalWrite(ledPins[i], LOW);
       }
 
-      else if(receivedData[1] == 1) // ON Button pressed
+      else if(receivedData[1] == 1) // Button Related -- ON Button is pressed
       {
-        for(int i=0; i<3; i++)
-          if(receivedData[i+2] == 1) 
+        for(int i = 0; i < 3; i++)
+          if(receivedData[i+2] == 1)  // Example array: {header,1,1, L1, L2, L3}
             digitalWrite(ledPins[i], HIGH);
       }
 
-      else if(receivedData[1] == 2) // TOGGLE Button pressed
+      else if(receivedData[1] == 2) // Button Related -- TOGGLE Button is pressed
       {
-        for(int i=0; i<3; i++)
-          if(receivedData[i+2] == 1) 
+        for(int i = 0; i < 3; i++)
+          if(receivedData[i+2] == 1)  // Example array: {header,1,2, L1, L2, L3}
             digitalWrite(ledPins[i], !digitalRead(ledPins[i]));
       }
     }
-    
-    else if(receivedData[0] == 2) // pot reading
+
+    else if(receivedData[0] == 2) // Example array: {header,2,*, *, *, *}
     {
-      float voltage = analogRead(POTPIN);
-      Serial.write(voltage);
+      int voltage = analogRead(POTPIN); // 0 - 1023
+      Serial.println(voltage);
     }
+    
   }
+
 }
